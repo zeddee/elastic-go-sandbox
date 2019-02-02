5d69d5cfada9e896f1ef5296911c33ef50549ba9
Zed Tan <zed@nsdmdh.com>
2019-02-02 16:01:56 +0800

    added example three; updating via the _udpate api

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ func main() {
 	}
 
 	//exampleOne()
-	exampleTwo()
+	//exampleTwo()
+	exampleThree()
 
 	//a.getLoop()
 }
@@ -63,6 +64,36 @@ func exampleTwo() {
 	// MUST use POST method for IDless calls
 	a.Post("customer/_doc?pretty", payload2)
 	a.Get("customer")
+
+	a.Delete("customer")    // cleanup by deleting customer index
+	a.Get("_cat/indices?v") // get list of indexes/indices
+}
+
+// update a doc using the elasticsearch update endpoing
+func exampleThree() {
+	payload1 := `{"name": "John Doe"}`
+	a.Put("customer/_doc/1?pretty", payload1)
+	a.Get("customer/_doc/1")
+
+	// Make update call. Notice how the payload is different from a PUT call.
+	payload2 := `
+				{
+					"doc": { "name": "Jane Doe", "age": 20 }
+				}
+	`
+	a.Post("customer/_doc/1/_update?pretty", payload2)
+	a.Get("customer/_doc/1")
+
+	// Update api also allows programmatic updates to content of document
+	// "ctx._source" is the current document at the endpoing we're calling
+	// payload3 increments the "age" attribute of the document at endpoint we're calling
+	payload3 := `
+				{
+					"script": "ctx._source.age += 5"
+				}
+	`
+	a.Post("customer/_doc/1/_update?pretty", payload3)
+	a.Get("customer/_doc/1")
 
 	a.Delete("customer")    // cleanup by deleting customer index
 	a.Get("_cat/indices?v") // get list of indexes/indices
