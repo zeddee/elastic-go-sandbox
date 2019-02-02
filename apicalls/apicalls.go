02d648ba5b4372353f6fa729bb725fa4236e85bd
Zed Tan <zed@nsdmdh.com>
2019-02-02 14:00:01 +0800

    refactored rest calls to separate package; renamed for brevity and exported public functions

---
+++ apicalls/apicalls.go
@@ -0,0 +1,99 @@
+package apicalls
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// Put wraps a put call to the elasticsearch server REST API
+func Put(path string, data string) {
+	res, err := putThis(path, data)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("PUT result: %s\n", res)
+}
+
+// Get wraps a get call to the elasticsearch server REST API
+func Get(path string) {
+	val, err := getThis(path)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("%s\n", val)
+}
+
+// Delete wraps a delete call to the elasticsearch server REST API
+func Delete(path string) {
+	res, err := deleteThis(path)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("DELETE result: %s\n", res)
+}
+
+func getThis(path string) (resBody []byte, error error) {
+	return newRequest(http.MethodGet, path, "")
+}
+
+func putThis(path string, data string) (resBody []byte, error error) {
+	return newRequest(http.MethodPut, path, data)
+}
+
+func deleteThis(path string) (resBody []byte, error error) {
+	return newRequest(http.MethodDelete, path, "")
+}
+
+func newRequest(method string, path string, data string) ([]byte, error) {
+	req, err := http.NewRequest(method, os.Getenv("ELASTIC_BASEURL")+"/"+path, strings.NewReader(data))
+	if err != nil {
+		return nil, fmt.Errorf("could not make request: %s", err)
+	}
+	req.Header.Set("content-type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("could not get valid response: %s", err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not read body of response: %s", err)
+	}
+
+	return body, nil
+}
+
+// CLI wraps a for loop that starts getting input from stdin to parse as CLI commands
+func CLI() {
+	for {
+		fmt.Printf("Please enter the path to GET\n")
+		input := readStringStdin()
+		if input == "" {
+			break
+		}
+		val, err := getThis(input)
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Printf("Response: %s\n", val)
+	}
+}
+
+func readStringStdin() string {
+	reader := bufio.NewReader(os.Stdin)
+	inputVal, err := reader.ReadString('\n')
+	if err != nil {
+		log.Printf("invalid option: %v\n", err)
+		return ""
+	}
+
+	output := strings.TrimSuffix(inputVal, "\n") // Important!
+	return output
+}
--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
+	a "github.com/zeddee/elastic-go-sandbox/apicalls"
 )
 
 func main() {
@@ -17,12 +12,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	getOne("_cat/health?v")  // get cluster health
-	getOne("_cat/nodes?v")   // get node list
-	getOne("_cat/indices?v") // get list of indexes/indices
+	exampleOne()
+	//a.getLoop()
+}
 
-	putOne("customer?pretty", "") // Add an index
-	getOne("_cat/indices?v")      // get list of indexes/indices
+func exampleOne() {
+	a.Get("_cat/health?v")  // get cluster health
+	a.Get("_cat/nodes?v")   // get node list
+	a.Get("_cat/indices?v") // get list of indexes/indices
+
+	a.Put("customer?pretty", "") // Add an index
+	a.Get("_cat/indices?v")      // get list of indexes/indices
 
 	// Add document to index, with payload
 	payload := `
@@ -30,94 +30,10 @@ func main() {
 				"name": "Jane Doe"
 			}
 			`
-	putOne("customer/_doc/1?pretty", payload)
+	a.Put("customer/_doc/1?pretty", payload)
 
-	getOne("customer") // get list of documents at customer
+	a.Get("customer") // get list of documents at customer
 
-	deleteOne("customer") // cleanup by deleting customer index
-	//getLoop()
-}
-
-func putOne(path string, data string) {
-	res, err := putThis(path, data)
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Printf("PUT result: %s\n", res)
-}
-
-func getOne(path string) {
-	val, err := getThis(path)
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Printf("%s\n", val)
-}
-
-func deleteOne(path string) {
-	res, err := deleteThis(path)
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Printf("DELETE result: %s\n", res)
-}
-
-func getThis(path string) (resBody []byte, error error) {
-	return newRequest(http.MethodGet, path, "")
-}
-
-func putThis(path string, data string) (resBody []byte, error error) {
-	return newRequest(http.MethodPut, path, data)
-}
-
-func deleteThis(path string) (resBody []byte, error error) {
-	return newRequest(http.MethodDelete, path, "")
-}
-
-func newRequest(method string, path string, data string) ([]byte, error) {
-	req, err := http.NewRequest(method, os.Getenv("ELASTIC_BASEURL")+"/"+path, strings.NewReader(data))
-	if err != nil {
-		return nil, fmt.Errorf("could not make request: %s", err)
-	}
-	req.Header.Set("content-type", "application/json")
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("could not get valid response: %s", err)
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, fmt.Errorf("could not read body of response: %s", err)
-	}
-
-	return body, nil
-}
-
-func getLoop() {
-	for {
-		fmt.Printf("Please enter the path to GET\n")
-		input := readStringStdin()
-		if input == "" {
-			break
-		}
-		val, err := getThis(input)
-		if err != nil {
-			log.Println(err)
-		}
-		fmt.Printf("Response: %s\n", val)
-	}
-}
-
-func readStringStdin() string {
-	reader := bufio.NewReader(os.Stdin)
-	inputVal, err := reader.ReadString('\n')
-	if err != nil {
-		log.Printf("invalid option: %v\n", err)
-		return ""
-	}
-
-	output := strings.TrimSuffix(inputVal, "\n") // Important!
-	return output
+	a.Delete("customer")    // cleanup by deleting customer index
+	a.Get("_cat/indices?v") // get list of indexes/indices
 }
